bc6bdd9d0b3828ebbd182007a2af552188fbe28a
George Karaszi <georgekaraszi@gmail.com>
2017-06-19 20:51:12 -0400

    Creating a new thread creates a new post

--- apps/mock_forum/lib/mock_forum/commands/thread_commands.ex
+++ apps/mock_forum/lib/mock_forum/commands/thread_commands.ex
@@ -8,6 +8,13 @@ defmodule MockForum.Commands.ThreadCommands do
         record_type: Thread, 
         associations: [:posts]
 
+    alias MockForum.Commands.PostCommands
+
+    def new_thread(record \\ %Thread{}, record_params \\ %{}) do
+        post = PostCommands.changeset
+        changeset(%Thread{posts: [post]})
+    end
+
     def create(subject, thread_params) do
         subject |> build_assoc(:threads) |> changeset(thread_params) |> Repo.insert
     end
