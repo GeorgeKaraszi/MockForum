c9fd65a2ea911719ada1971cea83308807c3dff7
George Karaszi <georgekaraszi@gmail.com>
2017-07-15 13:00:06 -0400

    Windows fix for imagemagick application name

--- apps/mock_forum/lib/mock_forum/models/avatar.ex
+++ apps/mock_forum/lib/mock_forum/models/avatar.ex
@@ -12,8 +12,13 @@
           Enum.member?(@extension_whitelist, file_extention)
       end
 
-      def transform(:original, _) do
-        {:convert, "-strip -thumbnail 75x75^ -gravity center -extent 75x75 -format png", :png}
+      def transform(version, params) do
+        magick_application = if os_windows?(), do: :magick, else: :convert
+        transform(magick_application, version, params)
+      end
+
+      def transform(program, :original, _) do
+        {program, "-strip -thumbnail 75x75^ -gravity center -extent 75x75 -format png", :png}
       end
       # Override the persisted filenames:
       def filename(_version, {_file, _scope}), do: "icon"
@@ -30,5 +35,16 @@
             |> :crypto.hash(scope.email)
             |> Base.encode16(case: :lower)
         "https://www.gravatar.com/avatar/#{email_hash}?s=75"
+      end
+
+      defp os_windows? do
+        case :os.type do
+          {:win32, _} ->
+            true
+          {:win64, _} ->
+            true
+          _ ->
+            false
+        end
       end
     end
